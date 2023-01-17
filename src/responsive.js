3d8a7cfc40a43c8b19f9840cfee7504f71423d59
Izzatbek Dadamirzayev <izzatbek_dadamirzaev@student.itpu.uz>
2023-01-17 09:04:52 +0500

    Deploying to netlify to check from mobile

---
+++ src/responsive.js
@@ -0,0 +1,10 @@
+const resnav = () =>{
+    const menu = document.querySelector(".menu-icon");
+    const nav = document.querySelector(".navbar-nav");
+
+    menu.addEventListener('click', () =>{
+        menu.classList.toggle('menu-icon_active');
+        nav.classList.toggle('.navbar-nav_active');
+    });
+}
+resnav();
